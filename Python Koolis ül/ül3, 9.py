32d295bb65a41cfa5e58aba404edfab3181f0623
Sten Ronan Siiman <sten1.siiman@gmail.com>
2026-02-03 16:17:27 +0200

    Ülesannete lahendamine

---
+++ Python Koolis ül/ül3, 9.py
@@ -0,0 +1,109 @@
+"""
+Koosta programm, mis "viskab täringut" kolm korda ehk väljastab ekraanile 3 juhusliku
+täringuviske tulemused. Et ekraanipilt oleks realistlikum, esita tulemused graafiliselt,
+selleks kasuta nn. ASCII graafikat (https://en.wikipedia.org/wiki/ASCII_art):
+imiteeri tekstisümbolite abil täringu külje kujutist.
+
+    Täiendamiseks:
+    Kasutaja võib alguses ise valida, mitu korda täringut visata.
+    Mängida võib mitu inimest, programmi alguses küsitakse inimeste nimesid.
+    Täringut imiteeritakse kolmemõõtmelisena.
+"""
+from random import randint
+
+
+dice_ascii = {
+
+    1: (
+
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+
+    ),
+
+    2: (
+
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+
+    ),
+
+    3: (
+
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+
+    ),
+
+    4: (
+
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+
+    ),
+
+    5: (
+
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+
+    ),
+
+    6: (
+
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+
+    ),
+
+}
+
+
+
+def throw_dice() -> tuple[int, str]:
+    throw_result = randint(1, 6)
+    return throw_result, dice_ascii[throw_result-1]
+
+def play_turn(throw_count, name="Player"):
+    for i in range(throw_count):
+        throw_result, dice_ascii = throw_dice()
+        for row in dice_ascii:
+            print(row)
+        print(f"{name} veeretas {throw_result}")
+
+
+if __name__ == '__main__':
+    throw_count = input("Mitut täringut soovid visata? ")
+    if throw_count.isdigit():
+        throw_count = int(throw_count)
+    else:
+        throw_count = 3
+    players_count = input("Mitu mängijat?")
+    if not players_count.isdigit():
+        play_turn(throw_count)
+    else:
+        players_count = int(players_count)
+        player_names = []
+        for i in range(players_count):
+            player_names.append(input(f"Sisesta {i + 1}. mängija nimi: "))
+        for name in player_names:
+            play_turn(throw_count, name)
+            input("Järgmise mängija kord. ENTER")
